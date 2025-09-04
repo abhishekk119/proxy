7a36d97c754a75f32cef0abf0df5424f8eec440f
Abhishek <abhishek.kabi.21@gmail.com>
2025-09-04 17:44:01 +0530

    Initial commit

---
+++ netlify/functions/zenquotes-proxy.js
@@ -0,0 +1,30 @@
+// netlify/functions/zenquotes-proxy.js
+const fetch = (...args) =>
+  import("node-fetch").then(({ default: fetch }) => fetch(...args));
+
+exports.handler = async (event) => {
+  try {
+    const response = await fetch("https://zenquotes.io/api/image");
+    const imageData = await response.text();
+
+    return {
+      statusCode: 200,
+      body: imageData,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Methods": "GET, OPTIONS",
+        "Content-Type": "text/plain",
+      },
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "Failed to fetch image" }),
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json",
+      },
+    };
+  }
+};
